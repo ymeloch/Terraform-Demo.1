4a43161e5eea251f83590d855b041419c76b9833
Yossi Meloch <yossi.meloch@gmail.com>
2021-04-25 20:12:22 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,12 +3,16 @@ terraform {
   # 0.12.26 as the minimum version, as that version added support for required_providers with source URLs, making it
   # forwards compatible with 0.13.x code.
   required_version = ">= 0.12.26"
-  triggers = {
-  always_run = timestamp()
-  }
 }
 
 # website::tag::1:: The simplest possible Terraform module: it just outputs "Hello, World!"
+
+resource "null_resource" "TE-Ent-Agent-Deploy" {
+  triggers = {
+    always_run = timestamp()
+  }
+}
+
 output "hello_world" {
   value = "Hello, World!"
 }
