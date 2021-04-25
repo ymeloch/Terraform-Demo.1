3f6610c5fc6223e901482d9756f6b23715f0925f
Yossi Meloch <yossi.meloch@gmail.com>
2021-04-25 20:06:31 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,6 +3,9 @@ terraform {
   # 0.12.26 as the minimum version, as that version added support for required_providers with source URLs, making it
   # forwards compatible with 0.13.x code.
   required_version = ">= 0.12.26"
+  triggers = {
+  always_run = timestamp()
+  }
 }
 
 # website::tag::1:: The simplest possible Terraform module: it just outputs "Hello, World!"
